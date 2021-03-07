63f23d80ae1353c2f19c0d49d74d4acd7c353d01
aybars0 <aybars-dikmeci@hotmail.com>
2021-03-07 04:41:32 +0300

    add about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-    def home
-        render html:'hello world'
+    def home#render html:'hello world'
     end
+    
+    def about#render html:'This is the About page'
+    end
+    
     
 end
